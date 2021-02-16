ad36c1874d42e02723aa958d216d98154d86becf
A7U <abdseyada04@gmail.com>
2021-02-16 02:39:45 +0100

    lots of updates including footer

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const path = require("path")
 
 const PORT = process.env.PORT || 80;
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
